d2e3401151b2674a34899df95963ee5cfd95219f
Ubuntu <azureuser@MaVM-TP.iskunyvxvp3e3fpke0cxmdiusg.zrhx.internal.cloudapp.net>
2026-01-15 08:50:48 +0000

    Complete infrastructure deployment: VNet, NSG, and VM with Nginx

--- deploy-vm.sh
+++ deploy-vm.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
-# Azure VM Deployment Script
+
+# 1. تعريف المتغيرات (Variables) لسهولة التعديل
 RG="TP-Azure-RG"
 LOC="switzerlandnorth"
+VNET_NAME="MonVNet"
+NSG_NAME="MonNSG"
+VM_NAME="MaVM-TP"
 
+echo "Starting Deployment in $LOC..."
+
+# 2. إنشاء مجموعة الموارد
 az group create --name $RG --location $LOC
-az network vnet create -g $RG -n MonVNet --subnet-name MonSubnet
-az network nsg create -g $RG -n MonNSG
-az network nsg rule create -g $RG --nsg-name MonNSG --name AllowSSH --priority 1000 --destination-port-ranges 22 --access Allow --protocol Tcp
-az network nic create -g $RG --name MaCarteReseau --vnet-name MonVNet --subnet MonSubnet --network-security-group MonNSG
-az vm create -g $RG --name MaVM-TP --nics MaCarteReseau --image Ubuntu2204 --admin-username azureuser --size Standard_B1s --generate-ssh-keys
+
+# 3. إنشاء الشبكة والشبكة الفرعية
+az network vnet create -g $RG -n $VNET_NAME --subnet-name MonSubnet --address-prefix 10.0.0.0/16 --subnet-prefix 10.0.1.0/24
+
+# 4. إنشاء مجموعة الأمان وفتح المنفذ 22 (SSH) والمنفذ 80 (HTTP)
+az network nsg create -g $RG -n $NSG_NAME
+az network nsg rule create -g $RG --nsg-name $NSG_NAME --name AllowSSH --priority 1000 --destination-port-ranges 22 --access Allow --protocol Tcp
+az network nsg rule create -g $RG --nsg-name $NSG_NAME --name AllowHTTP --priority 1001 --destination-port-ranges 80 --access Allow --protocol Tcp
+
+# 5. إنشاء بطاقة الشبكة وربطها بالـ NSG
+az network nic create -g $RG --name MaCarteReseau --vnet-name $VNET_NAME --subnet MonSubnet --network-security-group $NSG_NAME
+
+# 6. إنشاء الماكينة مع تثبيت Nginx تلقائياً عند التشغيل
+az vm create \
+  -g $RG \
+  -n $VM_NAME \
+  --nics MaCarteReseau \
+  --image Ubuntu2204 \
+  --admin-username azureuser \
+  --size Standard_B1s \
+  --custom-data "#cloud-config
+package_upgrade: true
+packages:
+  - nginx
+runcmd:
+  - systemctl start nginx" \
+  --generate-ssh-keys
+
+echo "Deployment Finished! Your VM is ready with Nginx."
